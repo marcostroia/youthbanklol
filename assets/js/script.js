45b0453514ea19a59ace205f886424129a70b803
Marco Stroia <marcostroia@rocketmail.com>
2018-01-03 22:49:25 +0200

    xd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -10,22 +9,3 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
-=======
-/* The sidenav */
-.sidenav {
-  height: 100%;
-  width: 200px;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background-color: #111;
-  overflow-x: hidden;
-  padding-top: 20px;
-}
-
-/* Page content */
-.main {
-  margin-left: 200px; /* Same as the width of the sidenav */
-}
->>>>>>> 0834262815c6bcf3dfe7a7581b2137e606c8394a
